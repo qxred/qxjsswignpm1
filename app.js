35a6a6506a08b800f0b5348a0503eea43fe594ec
qxred <qxessay@gmail.com>
2020-06-23 11:22:05 -0700

    init version

---
+++ app.js
@@ -0,0 +1,9 @@
+// import the extension via require
+var example = require("./example");
+
+// calling the global method
+var g = example.hello();
+
+//"use strict";
+
+//module.exports = require("./build/Release/example")
