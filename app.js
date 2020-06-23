a17e91f8f8e9a90b8408deddac9da8f0e414c71c
qxred <qxessay@gmail.com>
2020-06-23 11:23:34 -0700

    rm app

--- app.js
+++
@@ -1,9 +0,0 @@
-// import the extension via require
-var example = require("./example");
-
-// calling the global method
-var g = example.hello();
-
-//"use strict";
-
-//module.exports = require("./build/Release/example")
